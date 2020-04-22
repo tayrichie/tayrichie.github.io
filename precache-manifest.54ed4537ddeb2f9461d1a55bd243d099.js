da46cd8d6dfc8dbfc7c9e3b407704c31a97616af
Taylor Richards <taylor.richards@deque.com>
2020-04-22 10:46:09 -0400

    Updates

---
+++ precache-manifest.54ed4537ddeb2f9461d1a55bd243d099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3690f4f08262b2b05e774c3c8f8cc8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "330205c862b31d4cd732",
+    "url": "/static/css/main.dcf45b6e.chunk.css"
+  },
+  {
+    "revision": "aad5d9f19e482f456b91",
+    "url": "/static/js/2.74d6ceb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.74d6ceb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "330205c862b31d4cd732",
+    "url": "/static/js/main.207e103b.chunk.js"
+  },
+  {
+    "revision": "4258a70216d9efef44d1",
+    "url": "/static/js/runtime-main.cd533ead.js"
+  }
+]);
